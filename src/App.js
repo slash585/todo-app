4a7d795a6d0a8eb6f0cc2d8573715689e10ce5c6
slash585 <slsh585@gmail.com>
2022-02-16 18:12:22 +0300

    Create all components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import "./App.css"
+import Header from "./components/Header"
+import Content from "./components/Content"
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
-  );
+    <>
+      <section className="todoapp">
+        <Header />
+        <Content />
+      </section>
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
